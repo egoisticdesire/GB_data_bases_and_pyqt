e47bc0ee4f983e1d85738cff7de6bb0e01dceb46
egoistic_desire <kfkfjqjq@mail.ru>
2022-08-07 15:27:45 +0400

    hw1

---
+++ l1/task_3.py
@@ -0,0 +1,10 @@
+from tabulate import tabulate
+from task_2 import host_range_ping
+
+
+def host_range_ping_tab():
+    print(tabulate([host_range_ping(True)], headers='keys', tablefmt='pipe', stralign='center'))
+
+
+if __name__ == '__main__':
+    host_range_ping_tab()
